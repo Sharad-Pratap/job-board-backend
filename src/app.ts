01b74d3912b4720f9a863aa269355478656ccfa5
Sharad Pratap Singh <108497136+Sharad-Pratap@users.noreply.github.com>
2024-10-20 00:24:17 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,3 @@ app.use(cors<Request>());
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-
